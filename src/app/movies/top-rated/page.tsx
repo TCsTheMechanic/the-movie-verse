df9d12e39058cbc4cd259263e2020d1a0f795511
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2023-07-19 21:49:25 -0300

    Create top rated movies page and request

---
+++ src/app/movies/top-rated/page.tsx
@@ -0,0 +1,23 @@
+import { getTopRatedMovies } from "@/app/api/getTopRatedMovies"
+import { IMAGES_BASE_URL } from "@/app/api/routes"
+import MovieCard from "@/app/components/movieCard/movieCard"
+import MoviesList from "@/app/components/moviesList/moviesList"
+
+export default async function TopRatedMovies() {
+  const movies = await getTopRatedMovies()
+
+  return (
+    <MoviesList listType="Top Rated">
+      {movies.results.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          imageUrl={IMAGES_BASE_URL + movie.poster_path}
+          alt={movie.title + " image"}
+          title={movie.title}
+          date={movie.release_date}
+        />
+      ))}
+    </MoviesList>
+  )
+}
