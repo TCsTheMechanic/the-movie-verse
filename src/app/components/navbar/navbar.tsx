6116edf68954f3cc69413934fd80b8c095dc3499
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2023-07-20 09:43:55 -0300

    Add sidebar icons

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -10,12 +10,15 @@ export default function Navbar({
   setIsSidebarOpen: React.Dispatch<SetStateAction<boolean>>
 }) {
   return (
-    <div className="flex justify-between bg-zinc-900 w-screen">
+    <div className="flex justify-between bg-zinc-900 w-screen p-3">
       <Link href="/" className="flex space-x-3">
         <p>TMV</p>
         <p>The Movie Verse</p>
       </Link>
-      <div onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+      <div
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        className="cursor-pointer"
+      >
         {isSidebarOpen ? <X /> : <Menu />}
       </div>
     </div>
--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,19 +1,33 @@
+import { Star, Monitor, Film, Users } from "lucide-react"
 import Link from "next/link"
 
 export default function Sidebar() {
   return (
-    <aside className="flex flex-col bg-zinc-900 absolute right-0 top-6">
-      <Link href="/" className="p-3 border-y border-zinc-800">
-        Now Playing
+    <aside className="flex flex-col bg-zinc-900 absolute right-0 top-12">
+      <Link href="/" className="flex space-x-2 p-3 border-y border-zinc-800">
+        <Monitor />
+        <p>Now Playing</p>
       </Link>
-      <Link href="/movies/top-rated" className="p-3 border-y border-zinc-800">
-        Top Rated
+      <Link
+        href="/movies/top-rated"
+        className="flex space-x-2 p-3 border-y border-zinc-800"
+      >
+        <Star />
+        <p>Top Rated</p>
       </Link>
-      <Link href="/movies/popular" className="p-3 border-y border-zinc-800">
-        Popular
+      <Link
+        href="/movies/popular"
+        className="flex space-x-2 p-3 border-y border-zinc-800"
+      >
+        <Users />
+        <p>Popular</p>
       </Link>
-      <Link href="/movies/upcoming" className="p-3 border-y border-zinc-800">
-        Upcoming
+      <Link
+        href="/movies/upcoming"
+        className="flex space-x-2 p-3 border-y border-zinc-800"
+      >
+        <Film />
+        <p>Upcoming</p>
       </Link>
     </aside>
   )
